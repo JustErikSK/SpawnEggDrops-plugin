fcf7d9006b1988741d8dd30401dd0fc416de3d24
juste <sebo.erik@gmail.com>
2024-11-05 19:59:29 +0100

    Initial commit, no code yet

---
+++ src/main/java/me/minecraft/plugin/SpawnEggDrops.java
@@ -0,0 +1,17 @@
+package me.minecraft.plugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class SpawnEggDrops extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
