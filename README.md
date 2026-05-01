48c17c79d180fcc09c3a4d1029af4cff12320777
justerik <sebo.erik@gmail.com>
2026-05-01 22:34:51 +0200

    Backport of the new 1.3 update for 1.21

---
+++ README.md
@@ -0,0 +1,15 @@
+# SpawnEggDrops Plugin
+All mobs have by default a 10% chance to drop their spawn egg when killed. This can be changed through a configuration file!
+
+You can find detailed information about this plugin on our Wiki https://with-rage-studios-plugins.fandom.com/wiki/SpawnEggDrops
+
+**INSTALLATION:**
+1. Use one of these options to download SpawnEggDrops plugin:
+- [GitHub releases] https://github.com/JustErikSK/SpawnEggDrops-plugin/releases
+- [Spigot] https://www.spigotmc.org/resources/spawneggdrops.120732/
+- [CurseForge] https://www.curseforge.com/minecraft/bukkit-plugins/spawneggdrops
+- [Modrinth] https://modrinth.com/plugin/spawneggdrops
+2. Drop the file in your server's plugins folder and start your server, plugin will automatically load.
+3. After first server start with our plugin, new folder with a config file will be created in your server's plugins folder. You can change the drop percentage and drop amount there.
+
+This plugin works on Minecraft versions 1.21 - 26.1.2 (the newest version has 2 editions, one for 26.1 and one for 1.21)
---
+++ src/main/java/net/withrage/spawnEggDrops121/SpawnEggDrops121.java
@@ -0,0 +1,101 @@
+package net.withrage.spawnEggDrops121;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.CreatureSpawnEvent;
+import org.bukkit.event.entity.EntityDeathEvent;
+import org.bukkit.event.entity.EntityTransformEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.persistence.PersistentDataType;
+import org.bukkit.plugin.java.JavaPlugin;
+
+import java.util.Arrays;
+import java.util.concurrent.ThreadLocalRandom;
+
+public final class SpawnEggDrops121 extends JavaPlugin implements Listener {
+
+    private NamespacedKey FROM_SPAWNER;
+
+    @Override
+    public void onEnable() {
+        Bukkit.getConsoleSender().sendMessage(ChatColor.DARK_GREEN + "SpawnEggDrops >> Plugin has been enabled!");
+        this.getServer().getPluginManager().registerEvents(this, this);
+        saveDefaultConfig();
+
+        FROM_SPAWNER = new NamespacedKey(this, "from_spawner");
+
+        FileConfiguration config = this.getConfig();
+        config.addDefault("spawn_egg_drop_percentage", 10);
+        config.addDefault("spawn_egg_drop_amount", 1);
+        config.addDefault("drops_from_spawners", true);
+        config.addDefault("blacklisted_mobs", Arrays.asList("PLAYER"));
+        getConfig().options().copyDefaults(true);
+        saveConfig();
+    }
+
+    @EventHandler
+    public void onMobSpawn(CreatureSpawnEvent e) {
+        if (e.getSpawnReason() == CreatureSpawnEvent.SpawnReason.SPAWNER) {
+            e.getEntity().getPersistentDataContainer().set(FROM_SPAWNER, PersistentDataType.BYTE, (byte) 1);
+        }
+    }
+
+    @EventHandler
+    public void onTransform(EntityTransformEvent e) {
+        if (!(e.getEntity() instanceof LivingEntity original)) return;
+        Byte flag = original.getPersistentDataContainer().get(FROM_SPAWNER, PersistentDataType.BYTE);
+        if (flag == null || flag != (byte) 1) return;
+
+        for (Entity result : e.getTransformedEntities()) {
+            if (result instanceof LivingEntity le) {
+                le.getPersistentDataContainer().set(FROM_SPAWNER, PersistentDataType.BYTE, (byte) 1);
+            }
+        }
+    }
+
+    private Material getSpawnEggMaterial(EntityType type) {
+        Material egg = Material.matchMaterial(type.name() + "_SPAWN_EGG");
+        return egg;
+    }
+
+    private boolean isBlacklisted(EntityType type) {
+        for (String mobName : getConfig().getStringList("blacklisted_mobs")) {
+            if (mobName.equalsIgnoreCase(type.name())) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @EventHandler
+    public void mobDeath(EntityDeathEvent e) {
+        LivingEntity ent = e.getEntity();
+        int roll = ThreadLocalRandom.current().nextInt(100);
+
+        boolean allowSpawnerDrops = this.getConfig().getBoolean("drops_from_spawners", true);
+        boolean fromSpawner = ent.getPersistentDataContainer().has(FROM_SPAWNER, PersistentDataType.BYTE);
+
+        if (fromSpawner && !allowSpawnerDrops) return;
+
+        int dropPercentage = this.getConfig().getInt("spawn_egg_drop_percentage", 10);
+        int dropAmount = this.getConfig().getInt("spawn_egg_drop_amount", 1);
+        if (dropPercentage > 100 || dropPercentage < 1) { dropPercentage = 10; }
+        if (dropAmount > 10 || dropAmount < 1) { dropAmount = 1; }
+
+        if (roll <= dropPercentage) {
+            Material eggMat = getSpawnEggMaterial(ent.getType());
+            if (eggMat == null) return;
+            if (isBlacklisted(ent.getType())) return;
+
+            e.getDrops().add(new ItemStack(eggMat, dropAmount));
+        }
+    }
+}
